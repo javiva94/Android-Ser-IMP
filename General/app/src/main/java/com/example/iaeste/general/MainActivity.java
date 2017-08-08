5f43f39707b4888e00784407402d97db0da5e434
Franco Nicolás Tripi <francotripi92@gmail.com>
2017-08-08 10:10:25 +0200

    Adds button to show map

--- General/app/src/main/java/com/example/iaeste/general/MainActivity.java
+++ General/app/src/main/java/com/example/iaeste/general/MainActivity.java
@@ -36,12 +36,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton showMapBtn = (FloatingActionButton) findViewById(R.id.showMapBtn);
+        showMapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
             }
         });
 
--- General/app/src/main/java/com/example/iaeste/general/MapsActivity.java
+++ General/app/src/main/java/com/example/iaeste/general/MapsActivity.java
@@ -1,15 +1,22 @@
 package com.example.iaeste.general;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,6 +34,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     double lat = 0.0;
     double lng = 0.0;
 
+    private GoogleApiClient mGoogleApiClient;
+
+    public static int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    public static int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,15 +59,79 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        myPosition();
+
+       // LatLng dorm = new LatLng(44.813567, 20.483488);
+       // CameraUpdate update = CameraUpdateFactory.newLatLngZoom(dorm, 15.0f);
+       // mMap.moveCamera(update);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION},0 );
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location actualLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        LatLng actualLocationLatLng = new LatLng(actualLocation.getLatitude(), actualLocation.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(actualLocationLatLng));
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,0,locListener);
+
+/*
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location actualLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        LatLng actualLocationLatLng = new LatLng(actualLocation.getLatitude(), actualLocation.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(actualLocationLatLng));
+
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney)); */
+    }
+
+
+/*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Do other setup activities here too, as described elsewhere in this tutorial.
+
+        // Build the Play services client for use by the Fused Location Provider and the Places API.
+        // Use the addApi() method to request the Google Places API and the Fused Location Provider.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity ,
+                        this /* OnConnectionFailedListener )
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+*/
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode ==0){
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                mMap.setMyLocationEnabled(true);
+            }
+        }
     }
 
     private void addMarker(double lat, double lng) {
